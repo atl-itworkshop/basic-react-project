8366a4f5cc8ab6b307ddf4ac1f5644a5fada68c7
Rakesh Jain <rakeshvora2007@gmail.com>
2020-03-24 23:06:16 -0400

    installed axios, get allProducts in log

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import axios from "axios";
+
+axios
+    .get("http://localhost:5001/api/v1/products")
+    .then(resp => {
+        return resp.data;
+    })
+    .then(response => {
+        console.log(response.data);
+    })
 
 const App = () => {
   return (
